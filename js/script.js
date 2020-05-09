1f22e3ca9a0965ed4be1acdf6a640089ec3bf678
Bakster <baksterone@gmail.com>
2020-05-09 15:42:39 +0300

    adapt 1024

--- js/script.js
+++ js/script.js
@@ -2,9 +2,19 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         items: 1,
         loop: true,
-        dots: false
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 1000
         
     }); 
   });
+
+  document.getElementById("burger").onclick = function(){
+    addMenu();
+};
+
+function addMenu(){
+    document.getElementById("menu-clone").classList.toggle("show");
+};
 
 
